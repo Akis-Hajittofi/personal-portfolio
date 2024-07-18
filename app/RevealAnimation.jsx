f17a22a5f8fb9728ba15b5ac8472ba7a21875750
Akis-Hajittofi <akis.hajittofi@hotmail.co.uk>
2024-07-18 19:05:12 +0100

    Highlight animation complete

--- app/RevealAnimation.jsx
+++ app/RevealAnimation.jsx
@@ -1,22 +1,54 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 "use client";
 
-import React from "react";
-import { motion } from "framer-motion";
+import React, { useEffect, useRef } from "react";
+import { motion, useInView, useAnimation } from "framer-motion";
 
 function RevealAnimation({ children, width = "fit-content" }) {
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: true });
+
+  const mainControls = useAnimation();
+  const highlightControls = useAnimation();
+
+  useEffect(() => {
+    if (isInView) {
+      mainControls.start("visible");
+      highlightControls.start("visible");
+    }
+  }, [isInView]);
+
   return (
-    <div style={{ position: "relative", width, overflow: "hidden" }}>
+    <div ref={ref} style={{ position: "relative", width, overflow: "hidden" }}>
       <motion.div
         variants={{
           hidden: { opacity: 0, y: 75 },
           visible: { opacity: 1, y: 0 },
         }}
         initial="hidden"
-        animate="visible"
+        animate={mainControls}
         transition={{ duration: 0.5, delay: 0.25 }}
       >
         {children}
       </motion.div>
+      <motion.div
+        variants={{
+          hidden: { left: 0 },
+          visible: { left: "100%" },
+        }}
+        initial="hidden"
+        animate={highlightControls}
+        transition={{ duration: 0.5, ease: "easeIn" }}
+        style={{
+          position: "absolute",
+          top: 4,
+          bottom: 4,
+          left: 0,
+          right: 0,
+          background: "oklch(var(--p))",
+          //   zIndex: 20,
+        }}
+      />
     </div>
   );
 }
--- app/page.js
+++ app/page.js
@@ -13,13 +13,16 @@ export default function Home() {
     <main className=" flex flex-col w-full h-full lg:w-[1000px] space-y-16">
       <div className="w-full text-base-content pt-12 pb-0 sm:pb-16 ">
         <div className="w-full gap-5 flex flex-wrap justify-start ">
-          <div className="w-36 h-36">
-            <Image
-              src={profilePic}
-              alt={"Picture of me"}
-              className="rounded-full"
-            />
-          </div>
+          <RevealAnimation>
+            <div className="w-36 h-36">
+              <Image
+                src={profilePic}
+                alt={"Picture of me"}
+                className="rounded-full"
+              />
+            </div>
+          </RevealAnimation>
+
           <div className="">
             <RevealAnimation>
               <h1 className="text-7xl lg:text-8xl font-[1000]">
@@ -28,26 +31,34 @@ export default function Home() {
               </h1>
             </RevealAnimation>
 
-            <p className="text-5xl font-thin mb-4">
-              I'm a{" "}
-              <span className="text-primary font-extrabold ">
-                Frontend Developer
-              </span>
-            </p>
+            <RevealAnimation>
+              <p className="text-5xl font-thin mb-4">
+                I'm a{" "}
+                <span className="text-primary font-extrabold ">
+                  Frontend Developer
+                </span>
+              </p>
+            </RevealAnimation>
           </div>
         </div>
         <div className="flex flex-col">
-          <p className="text-2xl font-light w-fit lg:p-0 lg:max-w-[800px]">
-            I turn complexity into simplicity, harnessing the power of elegant
-            design in React & Tailwind. I have experience from working in a
-            variety of professional projects. Also graduated from a Web-Dev
-            bootcamp.
-          </p>
-          <Link href="#contact">
-            <button className="btn btn-wide btn-primary text-lg my-4">
-              Contact Me
-            </button>
-          </Link>
+          <RevealAnimation>
+            <p className="text-2xl font-light w-fit lg:p-0 lg:max-w-[800px]">
+              I turn complexity into simplicity, harnessing the power of elegant
+              design in React & Tailwind. I have experience from working in a
+              variety of professional projects. Also graduated from a Web-Dev
+              bootcamp.
+            </p>
+          </RevealAnimation>
+
+          <RevealAnimation>
+            {" "}
+            <Link href="#contact">
+              <button className="btn btn-wide btn-primary text-lg my-4">
+                Contact Me
+              </button>
+            </Link>
+          </RevealAnimation>
         </div>
       </div>
       <div className="space-y-32">
@@ -57,27 +68,32 @@ export default function Home() {
         >
           <Divider text="About" />
           <div className="flex flex-wrap w-full text-xl font-light space-y-7 sm:space-y-0">
-            <div className="space-y-6 md:w-8/12 whitespace-pre-line">
-              I'm Phanakis, also known as <em>Akis. </em>{" "}
-              {`2+ years of React and Next.js experience gained through a coding bootcamp and professional projects. I'm passionate about creating intuitive and engaging interfaces that simplify complex tasks. I leverage the power of React and Tailwind CSS to bring designs to life, ensuring they are both visually appealing and user-friendly.
+            <RevealAnimation>
+              <div className="space-y-6 md:w-8/12 whitespace-pre-line">
+                I'm Phanakis, also known as <em>Akis. </em>{" "}
+                {`2+ years of React and Next.js experience gained through a coding bootcamp and professional projects. I'm passionate about creating intuitive and engaging interfaces that simplify complex tasks. I leverage the power of React and Tailwind CSS to bring designs to life, ensuring they are both visually appealing and user-friendly.
 
                 After completing the bootcamp a year ago, I have recently re-immersed myself in web development, actively honing my skills and expanding my knowledge. In the past two months alone, I have gained valuable experience, particularly in RTK Query and improving my knowledge of React's built-in hooks such as useContext for simple sharing of data and useRef for using DOM components.
                 
                 I'm eager to kickstart my career in frontend development with ambitions of progressing towards becoming a purposeful senior developer. Please feel free to browse my projects and connect with me!`}
-            </div>
-            <div className="md:w-4/12 md:pl-5">
-              <h2 className="text-2xl font-[850] mb-4">Skills</h2>
-              <div className="flex flex-wrap gap-2">
-                {skills.map((skill, index) => (
-                  <div
-                    className="font-semibold w-fit bg-neutral text-neutral-content rounded-full px-2 "
-                    key={index}
-                  >
-                    {skill}
-                  </div>
-                ))}
               </div>
-            </div>
+            </RevealAnimation>
+
+            <RevealAnimation>
+              <div className="md:w-4/12 md:pl-5">
+                <h2 className="text-2xl font-[850] mb-4">Skills</h2>
+                <div className="flex flex-wrap gap-2">
+                  {skills.map((skill, index) => (
+                    <div
+                      className="font-semibold w-fit bg-neutral text-neutral-content rounded-full px-2 "
+                      key={index}
+                    >
+                      {skill}
+                    </div>
+                  ))}
+                </div>
+              </div>
+            </RevealAnimation>
           </div>
         </div>
         <div id="projects" className="scroll-mt-32 text-base-content w-fit">
@@ -126,6 +142,16 @@ export default function Home() {
     </main>
   );
 }
+
+
+
+
+
+
+
+
+
+
 
 
 
